f10223393b9a7316dc1fc6e3ec19643eae5e6b76
Choko <choko@curioswitch.org>
2025-08-06 17:19:30 +0900

    Wire up recipe generation when adding

--- frontend/client/src/pages/recipes/add/+Page.tsx
+++ frontend/client/src/pages/recipes/add/+Page.tsx
@@ -1,67 +1,278 @@
-import { create, type MessageInitShape } from "@bufbuild/protobuf";
+import { create } from "@bufbuild/protobuf";
 import { useMutation } from "@connectrpc/connect-query";
 import {
-  type AddRecipeRequest_AddRecipeStepSchema,
+  type AddRecipeRequest,
+  type AddRecipeRequest_AddRecipeStep,
+  AddRecipeRequest_AddRecipeStepSchema,
   AddRecipeRequestSchema,
   addRecipe,
-  type IngredientSectionSchema,
+  generateRecipe,
+  type IngredientSection,
+  IngredientSectionSchema,
+  type RecipeIngredient,
   RecipeIngredientSchema,
 } from "@cookchat/frontend-api";
 import { Button } from "@heroui/button";
 import { Form } from "@heroui/form";
 import { Input, Textarea } from "@heroui/input";
-import { useCallback, useState } from "react";
+import {
+  type Dispatch,
+  type SetStateAction,
+  useCallback,
+  useState,
+} from "react";
 import { useDropzone } from "react-dropzone";
 import { useTranslation } from "react-i18next";
+import { HiTrash } from "react-icons/hi";
 import { navigate } from "vike/client/router";
 
-function IngredientInput() {
+function IngredientInput({
+  idx,
+  sectionIdx,
+  ingredient,
+  setRequest,
+}: {
+  idx: number;
+  sectionIdx?: number;
+  ingredient: RecipeIngredient;
+  setRequest: Dispatch<SetStateAction<AddRecipeRequest>>;
+}) {
   const { t } = useTranslation();
+  const onNameChange = useCallback(
+    (value: string) =>
+      setRequest((prev) => {
+        if (sectionIdx) {
+          const ingredients = [
+            ...prev.additionalIngredients[sectionIdx].ingredients,
+          ];
+          ingredients[idx] = {
+            ...ingredients[idx],
+            name: value,
+          };
+          return {
+            ...prev,
+            additionalIngredients: prev.additionalIngredients.map((sec, i) =>
+              i === sectionIdx ? { ...sec, ingredients } : sec,
+            ),
+          };
+        }
+        const ingredients = [...prev.ingredients];
+        ingredients[idx] = {
+          ...ingredients[idx],
+          name: value,
+        };
+        return { ...prev, ingredients };
+      }),
+    [idx, setRequest, sectionIdx],
+  );
+  const onQuantityChange = useCallback(
+    (value: string) =>
+      setRequest((prev) => {
+        if (sectionIdx) {
+          const ingredients = [
+            ...prev.additionalIngredients[sectionIdx].ingredients,
+          ];
+          ingredients[idx] = {
+            ...ingredients[idx],
+            quantity: value,
+          };
+          return {
+            ...prev,
+            additionalIngredients: prev.additionalIngredients.map((sec, i) =>
+              i === sectionIdx ? { ...sec, ingredients } : sec,
+            ),
+          };
+        }
+        const ingredients = [...prev.ingredients];
+        ingredients[idx] = {
+          ...ingredients[idx],
+          quantity: value,
+        };
+        return { ...prev, ingredients };
+      }),
+    [idx, setRequest, sectionIdx],
+  );
+  const onRemoveClick = useCallback(() => {
+    setRequest((prev) => {
+      if (sectionIdx) {
+        const ingredients = [
+          ...prev.additionalIngredients[sectionIdx].ingredients,
+        ];
+        ingredients.splice(idx, 1);
+        return {
+          ...prev,
+          additionalIngredients: prev.additionalIngredients.map((sec, i) =>
+            i === sectionIdx ? { ...sec, ingredients } : sec,
+          ),
+        };
+      }
+      const ingredients = [...prev.ingredients];
+      ingredients.splice(idx, 1);
+      return { ...prev, ingredients };
+    });
+  }, [idx, setRequest, sectionIdx]);
 
   return (
     <div className="flex items-center gap-2">
       <Input
-        name="ingredient"
         label={t("Ingredient")}
         labelPlacement="outside"
+        value={ingredient.name}
+        onValueChange={onNameChange}
       />
-      <Input name="quantity" label={t("Quantity")} labelPlacement="outside" />
+      <Input
+        label={t("Quantity")}
+        labelPlacement="outside"
+        value={ingredient.quantity}
+        onValueChange={onQuantityChange}
+      />
+      <Button onPress={onRemoveClick}>
+        <HiTrash className="size-14" />
+      </Button>
     </div>
   );
 }
 
-function IngredientSectionInput({ idx }: { idx: number }) {
+function IngredientSectionInput({
+  idx,
+  section,
+  setRequest,
+}: {
+  idx: number;
+  section: IngredientSection;
+  setRequest: Dispatch<SetStateAction<AddRecipeRequest>>;
+}) {
   const { t } = useTranslation();
-  const [numIngredients, setNumIngredients] = useState(1);
-  const onAddIngredient = () => {
-    setNumIngredients((prev) => prev + 1);
-  };
+  const setTitle = useCallback(
+    (name: string) =>
+      setRequest((prev) => {
+        const sections = [...(prev.additionalIngredients ?? [])];
+        sections[idx] = {
+          ...sections[idx],
+          title: name,
+        };
+        return { ...prev, additionalIngredients: sections };
+      }),
+    [idx, setRequest],
+  );
+  const addIngredient = useCallback(() => {
+    setRequest((prev) => {
+      const sections = [...(prev.additionalIngredients ?? [])];
+      const ingredients = [...(sections[idx]?.ingredients ?? [])];
+      ingredients.push(create(RecipeIngredientSchema, {}));
+      sections[idx] = {
+        ...sections[idx],
+        ingredients,
+      };
+      return { ...prev, additionalIngredients: sections };
+    });
+  }, [idx, setRequest]);
+
+  const removeSection = useCallback(() => {
+    setRequest((prev) => {
+      const sections = [...(prev.additionalIngredients ?? [])];
+      sections.splice(idx, 1);
+      return { ...prev, additionalIngredients: sections };
+    });
+  }, [idx, setRequest]);
+
   return (
-    <div>
-      {idx === 0 ? (
-        <h3>{t("Main Ingredients")}</h3>
-      ) : (
-        <Input
-          name="section"
-          label={t("Section Name")}
-          labelPlacement="outside"
+    <div className="border-1 w-full">
+      <Input
+        label={t("Section Name")}
+        labelPlacement="outside"
+        value={section.title}
+        onValueChange={setTitle}
+      />
+      {section.ingredients.map((ingredient, i) => (
+        <IngredientInput
+          // biome-ignore lint/suspicious/noArrayIndexKey: input form
+          key={i}
+          idx={i}
+          sectionIdx={idx}
+          ingredient={ingredient}
+          setRequest={setRequest}
         />
-      )}
-      {Array.from({ length: numIngredients }).map((_, i) => (
-        // biome-ignore lint/suspicious/noArrayIndexKey: input form
-        <IngredientInput key={i} />
       ))}
-      <Button className="mt-4" onPress={onAddIngredient}>
+      <div>
+        <Button className="mt-4" onPress={addIngredient}>
+          {t("Add Ingredient")}
+        </Button>
+      </div>
+      <div>
+        <Button className="mt-4" onPress={removeSection}>
+          {t("Remove Section")}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function MainIngredientsInput({
+  ingredients,
+  setRequest,
+}: {
+  ingredients: RecipeIngredient[];
+  setRequest: Dispatch<SetStateAction<AddRecipeRequest>>;
+}) {
+  const { t } = useTranslation();
+  const addIngredient = useCallback(() => {
+    setRequest((prev) => {
+      const ingredients = [...(prev.ingredients ?? [])];
+      ingredients.push(create(RecipeIngredientSchema, {}));
+      return { ...prev, ingredients };
+    });
+  }, [setRequest]);
+  return (
+    <div className="border-1 w-full">
+      <h3>{t("Main Ingredients")}</h3>
+      {ingredients.map((ingredient, i) => (
+        <IngredientInput
+          // biome-ignore lint/suspicious/noArrayIndexKey: input form
+          key={i}
+          idx={i}
+          ingredient={ingredient}
+          setRequest={setRequest}
+        />
+      ))}
+      <Button className="mt-4" onPress={addIngredient}>
         {t("Add Ingredient")}
       </Button>
     </div>
   );
 }
 
-function Step() {
+function Step({
+  idx,
+  step,
+  setRequest,
+}: {
+  idx: number;
+  step: AddRecipeRequest_AddRecipeStep;
+  setRequest: Dispatch<SetStateAction<AddRecipeRequest>>;
+}) {
   const { t } = useTranslation();
 
-  const [previewURL, setPreviewURL] = useState<string | undefined>(undefined);
+  const setDescription = useCallback(
+    (description: string) =>
+      setRequest((prev) => {
+        const steps = [...(prev.steps ?? [])];
+        steps[idx] = {
+          ...steps[idx],
+          description,
+        };
+        return { ...prev, steps };
+      }),
+    [idx, setRequest],
+  );
+
+  const removeStep = useCallback(() => {
+    setRequest((prev) => {
+      const steps = [...(prev.steps ?? [])];
+      steps.splice(idx, 1);
+      return { ...prev, steps };
+    });
+  }, [idx, setRequest]);
 
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
@@ -71,39 +282,43 @@ function Step() {
     onDrop: async (acceptedFiles) => {
       if (acceptedFiles.length > 0) {
         const file = acceptedFiles[0];
-        const url = URL.createObjectURL(file);
-        setPreviewURL(url);
+
+        const url = await readFile(file);
+
+        setRequest((prev) => {
+          const steps = [...(prev.steps ?? [])];
+          steps[idx] = {
+            ...steps[idx],
+            imageDataUrl: url,
+          };
+          return { ...prev, steps };
+        });
       }
     },
   });
 
-  const onImgLoad = useCallback(() => {
-    if (previewURL) {
-      URL.revokeObjectURL(previewURL);
-    }
-  }, [previewURL]);
-
   return (
-    <>
+    <div className="border-1 w-full">
       <Textarea
-        name="step-description"
         label={t("Step Description")}
         labelPlacement="outside"
+        value={step.description}
+        onValueChange={setDescription}
       />
       <label htmlFor="step-image">{t("Step Image")}</label>
       <div className="flex gap-4 items-center">
         <div className="mt-2 p-4 rounded-xl bg-gray-100" {...getRootProps()}>
-          <input name="step-image" {...getInputProps()} />
+          <input {...getInputProps()} />
           <p>Drag 'n' drop or click to select image</p>
         </div>
         <img
-          className="h-100"
-          alt="Step preview"
-          src={previewURL}
-          onLoad={onImgLoad}
+          className={step.imageDataUrl ? "h-100" : "hidden"}
+          alt="Step"
+          src={step.imageDataUrl}
         />
       </div>
-    </>
+      <Button onPress={removeStep}>{t("Remove Step")}</Button>
+    </div>
   );
 }
 
@@ -123,13 +338,73 @@ async function readFile(file: File): Promise<string> {
 }
 
 export default function Page() {
-  const [numSections, setNumSections] = useState(1);
-  const onAddSection = useCallback(() => {
-    setNumSections((prev) => prev + 1);
+  const [prompt, setPrompt] = useState("");
+
+  const doGenerateRecipe = useMutation(generateRecipe, {
+    onSuccess: (res) => {
+      if (res.addRecipeRequest) {
+        setRequest(res.addRecipeRequest);
+      }
+    },
+  });
+
+  const onGenerateClick = useCallback(() => {
+    doGenerateRecipe.mutate({
+      prompt,
+    });
+  }, [doGenerateRecipe, prompt]);
+
+  const [request, setRequest] = useState<AddRecipeRequest>(
+    create(AddRecipeRequestSchema, {}),
+  );
+  const setTitle = useCallback((title: string) => {
+    setRequest((prev) => ({ ...prev, title }));
   }, []);
-  const [numSteps, setNumSteps] = useState(1);
-  const onAddStep = useCallback(() => {
-    setNumSteps((prev) => prev + 1);
+  const setDescription = useCallback((description: string) => {
+    setRequest((prev) => ({ ...prev, description }));
+  }, []);
+  const setServingSize = useCallback((servingSize: string) => {
+    setRequest((prev) => ({ ...prev, servingSize }));
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 1,
+    accept: {
+      "image/*": [],
+    },
+    onDrop: async (acceptedFiles) => {
+      if (acceptedFiles.length > 0) {
+        const file = acceptedFiles[0];
+
+        const url = await readFile(file);
+
+        setRequest((prev) => {
+          return {
+            ...prev,
+            mainImageDataUrl: url,
+          };
+        });
+      }
+    },
+  });
+
+  const addStep = useCallback(() => {
+    setRequest((prev) => {
+      const steps = [...(prev.steps ?? [])];
+      steps.push(create(AddRecipeRequest_AddRecipeStepSchema, {}));
+      return {
+        ...prev,
+        steps,
+      };
+    });
+  }, []);
+
+  const addIngredientSection = useCallback(() => {
+    setRequest((prev) => {
+      const sections = [...(prev.additionalIngredients ?? [])];
+      sections.push(create(IngredientSectionSchema, {}));
+      return { ...prev, additionalIngredients: sections };
+    });
   }, []);
 
   const doAddRecipe = useMutation(addRecipe, {
@@ -141,148 +416,79 @@ export default function Page() {
   const onSubmit = useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const data = new FormData(e.currentTarget);
-
-      const request: MessageInitShape<typeof AddRecipeRequestSchema> = {};
-      request.ingredients = [];
-      request.additionalIngredients = [];
-      request.steps = [];
-
-      let currentSection:
-        | MessageInitShape<typeof IngredientSectionSchema>
-        | undefined;
-      let currentStep:
-        | MessageInitShape<typeof AddRecipeRequest_AddRecipeStepSchema>
-        | undefined;
-      let currentIngredient:
-        | MessageInitShape<typeof RecipeIngredientSchema>
-        | undefined;
-
-      for (const [key, value] of data.entries()) {
-        console.log(`${key}: ${value}`);
-        switch (key) {
-          case "title":
-            request.title = String(value);
-            break;
-          case "description":
-            request.description = String(value);
-            break;
-          case "servingSize":
-            request.servingSize = String(value);
-            break;
-          case "image":
-            request.mainImageDataUrl = await readFile(value as File);
-            break;
-          case "section":
-            if (currentIngredient) {
-              if (currentSection) {
-                currentSection.ingredients?.push(
-                  create(RecipeIngredientSchema, currentIngredient),
-                );
-              } else {
-                request.ingredients.push(currentIngredient);
-              }
-              currentIngredient = undefined;
-            }
-            if (currentSection) {
-              request.additionalIngredients.push(currentSection);
-            }
-            currentSection = {
-              title: String(value),
-            };
-            currentSection.ingredients = [];
-            break;
-          case "ingredient":
-            if (currentIngredient) {
-              if (currentSection) {
-                currentSection.ingredients?.push(
-                  create(RecipeIngredientSchema, currentIngredient),
-                );
-              } else {
-                request.ingredients.push(currentIngredient);
-              }
-            }
-            currentIngredient = {
-              name: String(value),
-            };
-            break;
-          case "quantity":
-            if (currentIngredient) {
-              currentIngredient.quantity = String(value);
-            }
-            break;
-          case "step-description":
-            if (currentStep) {
-              request.steps.push(currentStep);
-            }
-            currentStep = {
-              description: String(value),
-            };
-            break;
-          case "step-image":
-            if (currentStep) {
-              currentStep.imageDataUrl = await readFile(value as File);
-            }
-            break;
-          default:
-        }
-      }
-      if (currentIngredient) {
-        if (currentSection) {
-          currentSection.ingredients?.push(
-            create(RecipeIngredientSchema, currentIngredient),
-          );
-        } else {
-          request.ingredients.push(
-            create(RecipeIngredientSchema, currentIngredient),
-          );
-        }
-      }
-      if (currentSection) {
-        request.additionalIngredients.push(currentSection);
-      }
-      if (currentStep) {
-        request.steps.push(currentStep);
-      }
-      doAddRecipe.mutate(create(AddRecipeRequestSchema, request));
+      doAddRecipe.mutate(request);
     },
-    [doAddRecipe],
+    [doAddRecipe, request],
   );
 
   return (
     <div className="p-4">
+      <Textarea
+        label="Generate Recipe Prompt"
+        value={prompt}
+        onValueChange={setPrompt}
+      />
+      <Button
+        className="mt-2"
+        onPress={onGenerateClick}
+        disabled={doGenerateRecipe.isPending}
+      >
+        Generate Recipe
+      </Button>
+      {doGenerateRecipe.isPending && <div>Generating recipe...</div>}
+      <hr />
       <Form onSubmit={onSubmit}>
-        <Input name="title" label="Title" labelPlacement="outside-top" />
+        <Input
+          label="Title"
+          labelPlacement="outside-top"
+          value={request.title}
+          onValueChange={setTitle}
+        />
         <Textarea
-          name="description"
           label="Description"
           labelPlacement="outside"
+          value={request.description}
+          onValueChange={setDescription}
         />
         <Input
-          name="servingSize"
           label="Serving Size"
           labelPlacement="outside-top"
+          value={request.servingSize}
+          onValueChange={setServingSize}
         />
-        <Input
-          type="file"
-          accept="image/*"
-          name="image"
-          label="Main Image"
-          labelPlacement="outside"
+        <div className="flex gap-4 items-center">
+          <div className="mt-2 p-4 rounded-xl bg-gray-100" {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop or click to select image</p>
+          </div>
+          <img
+            className={request.mainImageDataUrl ? "h-100" : "hidden"}
+            alt="Recipe"
+            src={request.mainImageDataUrl}
+          />
+        </div>
+        <MainIngredientsInput
+          ingredients={request.ingredients}
+          setRequest={setRequest}
         />
-        {Array.from({ length: numSections }).map((_, i) => (
-          // biome-ignore lint/suspicious/noArrayIndexKey: input form
-          <IngredientSectionInput key={i} idx={i} />
+        {request.additionalIngredients.map((section, i) => (
+          <IngredientSectionInput
+            // biome-ignore lint/suspicious/noArrayIndexKey: input form
+            key={i}
+            idx={i}
+            section={section}
+            setRequest={setRequest}
+          />
         ))}
-        <Button className="mt-4" onPress={onAddSection}>
+        <Button className="mt-4" onPress={addIngredientSection}>
           Add Section
         </Button>
         <h3 className="mt-4">Steps</h3>
-        {Array.from({ length: numSteps }).map((_, i) => (
+        {request.steps?.map((step, i) => (
           // biome-ignore lint/suspicious/noArrayIndexKey: input form
-          <Step key={i} />
+          <Step key={i} idx={i} step={step} setRequest={setRequest} />
         ))}
-        <Button className="mt-4" onPress={onAddStep}>
+        <Button className="mt-4" onPress={addStep}>
           Add Step
         </Button>
         <Button type="submit" className="mt-4" disabled={doAddRecipe.isPending}>
