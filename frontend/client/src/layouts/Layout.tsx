3d427bfd7d11902054a93c045f901e94e4251816
Choko <choko@curioswitch.org>
2025-08-14 17:34:39 +0900

    Update footer (#138)

--- frontend/client/src/layouts/Layout.tsx
+++ frontend/client/src/layouts/Layout.tsx
@@ -1,7 +1,10 @@
 import { Badge } from "@heroui/badge";
 import { Divider } from "@heroui/divider";
 import { Link } from "@heroui/link";
-import { HiHeart, HiShoppingCart, HiUser, HiViewList } from "react-icons/hi";
+import { useTranslation } from "react-i18next";
+import { FaBookmark, FaCalendarAlt } from "react-icons/fa";
+import { HiShoppingCart } from "react-icons/hi";
+import { PiForkKnifeFill } from "react-icons/pi";
 import { twMerge } from "tailwind-merge";
 import { usePageContext } from "vike-react/usePageContext";
 
@@ -9,6 +12,8 @@ import { ChatButton } from "../components/ChatButton";
 import { useCartStore, useChatStore } from "../stores";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { t } = useTranslation();
+
   const pageContext = usePageContext();
   const path = pageContext.urlPathname;
 
@@ -20,41 +25,63 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="container mx-auto min-h-screen max-w-full md:w-4xl prose md:prose-lg pb-16 bg-white">
         {children}
       </div>
-      <div className="fixed bottom-0 w-full h-16 md:h-24 bg-white z-50">
+      <div className="fixed bottom-0 w-full h-24 md:h-24 bg-white z-50">
         <Divider />
-        <div className="px-4 flex items-center justify-between h-full w-full">
-          <Link href="/">
-            <HiViewList
-              className={twMerge(
-                "size-8 md:size-12",
-                path === "/" ? "text-orange-400" : "text-gray-400",
-              )}
-            />
+        <div className="px-12 flex items-center justify-between h-full w-full">
+          <Link
+            href="/"
+            className={twMerge(
+              "flex flex-col gap-1",
+              path === "/" || path.startsWith("/recipes/")
+                ? "text-orange-400"
+                : "text-gray-400",
+            )}
+          >
+            <PiForkKnifeFill className="size-8 md:size-12" />
+            <div>{t("Recipe")}</div>
           </Link>
-          <HiHeart className="size-8 md:size-12 text-gray-400" />
+          <Link
+            href="/plans"
+            className={twMerge(
+              "flex flex-col gap-1",
+              path.startsWith("/plans") ? "text-orange-400" : "text-gray-400",
+            )}
+          >
+            <FaCalendarAlt className="size-8 md:size-12" />
+            <div>{t("Plan")}</div>
+          </Link>
           <ChatButton
             className={
-              chatStore.currentRecipeId ? "mb-20 md:mb-40" : "invisible"
+              chatStore.currentRecipeId ? "fixed bottom-32 right-8" : "hidden"
             }
             recipeId={chatStore.currentRecipeId}
             navigateToStep={chatStore.navigateToStep}
             prompt={chatStore.prompt}
           />
-          <Link href="/cart">
+          <Link
+            href="/settings"
+            className={twMerge(
+              "flex flex-col gap-1",
+              path === "/settings" ? "text-orange-400" : "text-gray-400",
+            )}
+          >
+            <FaBookmark className="size-8 md:size-12" />
+            <div>{t("Bookmarks")}</div>
+          </Link>
+          <Link
+            href="/cart"
+            className={twMerge(
+              "flex flex-col gap-1",
+              path === "/cart" ? "text-orange-400" : "text-gray-400",
+            )}
+          >
             <Badge
               content={cart.recipes.length}
               isInvisible={cart.recipes.length === 0}
             >
-              <HiShoppingCart
-                className={twMerge(
-                  "size-8 md:size-12",
-                  path === "/cart" ? "text-orange-400" : "text-gray-400",
-                )}
-              />
+              <HiShoppingCart className="size-8 md:size-12" />
             </Badge>
-          </Link>
-          <Link href="/settings">
-            <HiUser className="size-8 md:size-12 text-gray-400" />
+            <div>{t("Cart")}</div>
           </Link>
         </div>
       </div>
