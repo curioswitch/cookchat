02cbdb982d27ee863903d315fd5d1982b3cbc454
Choko <choko@curioswitch.org>
2025-08-27 17:32:24 +0900

    Fix

--- frontend/client/src/components/ChatButton/index.tsx
+++ frontend/client/src/components/ChatButton/index.tsx
@@ -283,44 +283,42 @@ export function ChatButton({
   }, [stream]);
 
   return (
-    <div>
-      <div
+    <div
+      className={twMerge(
+        "relative flex flex-col items-center gap-2 text-primary font-semibold",
+        className,
+      )}
+    >
+      {/* Play animation after assistant finishes speaking. */}
+      {playing && waiting && <span className="mic-ping" />}
+      <button
+        type="button"
+        onClick={onClick}
         className={twMerge(
-          "relative flex flex-col items-center gap-2 text-primary font-semibold",
-          className,
+          playing && speaking
+            ? "bg-gray-400"
+            : "bg-linear-to-r from-[#f97316] to-[#fb923c]",
+          "z-10 rounded-full size-18 md:size-50 flex items-center justify-center cursor-pointer",
         )}
       >
-        {/* Play animation after assistant finishes speaking. */}
-        {playing && waiting && <span className="mic-ping" />}
-        <button
-          type="button"
-          onClick={onClick}
-          className={twMerge(
-            playing && speaking
-              ? "bg-gray-400"
-              : "bg-linear-to-r from-[#f97316] to-[#fb923c]",
-            "z-10 rounded-full size-18 md:size-50 flex items-center justify-center cursor-pointer",
-          )}
-        >
-          {!playing ? (
-            <HiMicrophone className="text-white size-6 md:size-16" />
-          ) : waiting ? (
-            <div className="text-white text-lg">話して</div>
-          ) : (
-            <HiStop className="text-white size-6 md:size-16" />
-          )}
-        </button>
-        <button
-          type="button"
-          onClick={onClick}
-          className={twMerge(
-            "bg-[#ea580c] rounded-2xl py-1 px-2 text-white",
-            playing && "invisible",
-          )}
-        >
-          <div className="text-tiny font-light">COOPiiと話す</div>
-        </button>
-      </div>
+        {!playing ? (
+          <HiMicrophone className="text-white size-6 md:size-16" />
+        ) : waiting ? (
+          <div className="text-white text-lg">話して</div>
+        ) : (
+          <HiStop className="text-white size-6 md:size-16" />
+        )}
+      </button>
+      <button
+        type="button"
+        onClick={onClick}
+        className={twMerge(
+          "rounded-2xl py-1 px-2 bg-[#ea580c] font-light text-white !text-tiny",
+          playing && "invisible",
+        )}
+      >
+        COOPiiと話す
+      </button>
     </div>
   );
 }
