ab83a7f65f7f14d2aa18b081e0d6162ee30a185c
Choko <choko@curioswitch.org>
2026-02-18 17:36:30 +0900

    Fix tasks original auth middleware

--- tasks/server/main.go
+++ tasks/server/main.go
@@ -93,11 +93,12 @@ func setupServer(ctx context.Context, conf *config.Config, s *server.Server) err
 	io := file.NewIO(storage, publicBucket)
 	processor := recipegen.NewPostProcessor(genAI, firestore, image.NewWriter(io))
 
-	mux.Use(func(http.Handler) http.Handler {
-		return http.HandlerFunc(func(_ http.ResponseWriter, r *http.Request) {
+	mux.Use(func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if orig := r.Header.Get("X-Original-Authorization"); orig != "" {
 				r.Header.Set("Authorization", orig)
 			}
+			h.ServeHTTP(w, r)
 		})
 	})
 
