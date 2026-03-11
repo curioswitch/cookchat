d45af4bdc951c5782bc5bb490a937cc8d87c77c0
Choko <choko@curioswitch.org>
2026-03-11 16:04:24 +0900

    Set image URLs when filling plan (#317)

--- tasks/server/internal/llm/prompt.go
+++ tasks/server/internal/llm/prompt.go
@@ -10,7 +10,8 @@ func GenerateExecutionPlanPrompt() string {
 const generateExecutionPlanPrompt = `You help users schedule meal plans. The user has selected recipes to cook together as
 a meal. Provide an execution plan for the recipes. Group steps from different recipes together into step groups, trying to allow for
 parallel execution of steps within a group where possible. It is fine for a group to contain only a single step. Copy the description
-and image URL as is into the step within a group - do not add any prefix to the description. If there is any note for execution within
+and image URL as is into the step within a group - do not add any prefix to the description. The image URL for a step is the same
+indexed item within the s array of the recipe. If there is any note for execution within
 a step group, such as which step to execute while waiting on another, provide it. If there are any notes to consider when preparing the
 entire plan, return them. Only return text in Japanese.
 `
